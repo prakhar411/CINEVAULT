4355d9b17dc4420d9719e7cb62ff1329830480f6
prakhar411 <prakharchandra492@gmail.com>
2026-06-25 13:10:54 +0530

    backend structure created

---
+++ CINEVAULT-BACKEND/src/main/java/com/cinevault/cinevault/CinevaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cinevault.cinevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinevaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinevaultBackendApplication.class, args);
+	}
+
+}
---
+++ CINEVAULT-BACKEND/src/test/java/com/cinevault/cinevault/CinevaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinevault.cinevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinevaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
